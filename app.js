b82abe0b0f2c8cf1fa02166b71a51dd55aabb40e
ram-Ranjan <ranjan.irl3107@gmail.com>
2024-05-22 18:46:35 +0530

    /product url mapped

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-
-const http = require("http");
-
 const express = require('express')
+
+const bodyParser = require('body-parser')
 const app = express();
 
 
-app.use((req, res, next) => {
+
+app.use(bodyParser.urlencoded({ extended:false }));
+
+app.use('/add-product',(req, res, next) => {
 
     console.log(" In the Middleware")
-    next();
+   res.send(`<form action="/product" method="POST"> <input type="text" name="title">  <button type="submit">Add</button></form>`)
 
 
 })
 
 
-app.use((req, res, next) => {
+app.use('/product',(req, res, next) => {
 
-    console.log(" In Another Middleware")
-    // res.send(`<h2>Hello From Express Js</h2>`)
-    res.send( { key1: value }) 
+    console.log(req.body)
+    res.redirect('/')
+
+})
+
+
+
+app.use('/',(req, res, next) => {
+
+    res.send(`<h2>Hello From Express Js</h2>`)
 
 
 })
-const server = http.createServer(app);
 
 
-server.listen(4000,'localhost', () => console.log("Handling Requests"))
+app.listen(3000)
+// const server = http.createServer(app);
+
+
+// server.listen(4000,'localhost', () => console.log("Handling Requests"))
 
