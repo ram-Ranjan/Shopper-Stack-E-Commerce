e6fce5b067e154a029c07d02d314e803628d3ea4
ram-Ranjan <ranjan.irl3107@gmail.com>
2024-05-24 11:46:37 +0530

    admin routes added with the separte filter

--- routes/admin.js
+++ routes/admin.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 
-
+//  /admin/add-product   => GET
 router.get('/add-product',(req, res, next) => {
 
     console.log(" In the Middleware")
-   res.send(`<form action="/product" method="POST"> <input type="text" name="title">  
+   res.send(`<form action="/admin/add-product" method="POST"> <input type="text" name="title">  
    <input type="text" name="productSize" placeholder="Product Size" required>
    <button type="submit">Add</button></form>`)
 
 
 })
 
-
-router.post('/product',(req, res, next) => {
+//  /admin/add-product   => POST
+router.post('/add-product',(req, res, next) => {
 
     console.log(req.body)
     res.redirect('/')
