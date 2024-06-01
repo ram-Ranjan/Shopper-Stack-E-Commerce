ba163841e7cc37f0918cceb52cbde72d1bd0183a
ram-Ranjan <ranjan.irl3107@gmail.com>
2024-06-01 13:35:46 +0530

    Added cart post request

--- controllers/shop.js
+++ controllers/shop.js
@@ -40,6 +40,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req,res,next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+
+  res.redirect('/cart')
+}
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
--- routes/shop.js
+++ routes/shop.js
@@ -16,6 +16,8 @@ router.get('/products/:productId')//dynamic route
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart',shopController.postCart);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
