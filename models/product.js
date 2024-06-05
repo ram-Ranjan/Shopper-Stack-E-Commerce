5814c0abe2e5734148b458af05ae4665087423ba
ram-Ranjan <ranjan.irl3107@gmail.com>
2024-06-05 10:08:52 +0530

    Finished Edit Functionality

--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id,title, imageUrl, description, price) {
+    this.id= id
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,13 +27,24 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
-    getProductsFromFile(products => {
+       getProductsFromFile(products => {
+      if(this.id){
+      const existingProductIndex = products.findIndex(prod => prod.id === this.id)
+      const updatedProducts  = [...products];
+      updatedProducts[existingProductIndex] = this;//this is the updated product
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        console.log(err);
+      });
+    }
+    else{
+      this.id = Math.random().toString();
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
+    }
     });
+  
   }
 
   static fetchAll(cb) {
