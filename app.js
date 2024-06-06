03e8a18686c3dbaaf18df198347a70ce284f3966
ram-Ranjan <ranjan.irl3107@gmail.com>
2024-06-06 13:26:11 +0530

    added sequalize

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const db = require('./util/database')
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -31,4 +31,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- util/database.js
+++ util/database.js
@@ -1,16 +1,25 @@
-const mysql = require('mysql2');
-//After execution of every query we should close a particular connection.
-// this could be problematic while executing a single connection.
+// const mysql = require('mysql2');
+// //After execution of every query we should close a particular connection.
+// // this could be problematic while executing a single connection.
+
+// //Rather than creating a single connection we create a connection pool which gives 
+// //a pool of connections to use.
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user:'root',
+//     database:'e-commerce',
+//     password:'root@321'
+// });
+// module.exports = pool.promise();
 
 
+//
+const Sequelize = require('sequelize');
 
-//Rather than creating a single connection we create a connection pool which gives 
-//a pool of connections to use.
-const pool = mysql.createPool({
-    host: 'localhost',
-    user:'root',
-    database:'e-commerce',
-    password:'root@321'
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
